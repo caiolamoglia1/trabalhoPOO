08360e51d992c0c29f33ac9a642e7c810df2365b
Gastão Santos <86205611+ghastsantos@users.noreply.github.com>
2024-10-23 20:30:07 -0300

    Update Notas.java

--- PJBL/Notas.java
+++ PJBL/Notas.java
@@ -26,29 +26,11 @@ public class Notas {
     class Usuario{
         private String usuario;
         private String senha;
-
-        // Alterado para definir a variável 'usuario'
+            
         public Usuario(String usuario, String senha) {
             this.usuario = usuario;
             this.senha = senha;
         }
-
-        // Removidos os parâmetros 'usuario' e 'senha' do método 'login'
-        public void login() {
-            String inputUsuario = JOptionPane.showInputDialog(null, "Usuário");
-            String inputSenha = JOptionPane.showInputDialog(null, "Senha");
-
-            System.out.println("Usuário digitado: " + inputUsuario);
-            System.out.println("Senha digitada: " + inputSenha);
-
-            // Comparação correta usando 'this.usuario' e 'this.senha'
-            if (inputUsuario != null && inputSenha != null && inputUsuario.equals(this.usuario) && inputSenha.equals(this.senha)) {
-                JOptionPane.showMessageDialog(null, "login autorizado");
-            } else {
-                JOptionPane.showMessageDialog(null, "login recusado");
-            }
-        }
-    }
 
     class Categorias {
         private String nomeCategoria;
@@ -61,7 +43,7 @@ public class Notas {
 
         public void adicionarNota(Notas nota) {
             listaDeNotas.add(nota);
-        }
+        
 
         public void exibirNotas() {
             System.out.println("Categoria: " + nomeCategoria);
