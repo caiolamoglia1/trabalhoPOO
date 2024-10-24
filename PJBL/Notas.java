1af4ee44c24ee97478fff447471cf9729d53ebc9
matheus saldanha <137432368+mathsldn@users.noreply.github.com>
2024-10-23 22:14:11 -0300

    Feito alterações em Notas

--- PJBL/Notas.java
+++ PJBL/Notas.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
+    import java.util.ArrayList;
 
-public class Notas {
+    public class Notas {
         private String titulo;
         private String conteudo;
 
@@ -22,39 +22,45 @@ public class Notas {
         }
     }
 
-    class Usuario{
-        private String usuario;
-        private String senha;
-            
-        public Usuario(String usuario, String senha) {
-            this.usuario = usuario;
-            this.senha = senha;
-        }
-
     class Categorias {
         private String nomeCategoria;
         private ArrayList<Notas> listaDeNotas;
+        private ArrayList<NotaFinalizada> listaDeNotasFinalizadas;
+        private ArrayList<NotaImportante> listaDeNotasImportantes;
 
-        public Categorias(String nomeCategoria) {
+
+        public Categorias(String nomeCategoria, ArrayList <Notas> listaDeNotas, ArrayList<NotaFinalizada> listaDeNotasFinalizadas, ArrayList<NotaImportante> listaDeNotasImportantes) {
             this.nomeCategoria = nomeCategoria;
             this.listaDeNotas = new ArrayList<>();
+            this.listaDeNotasFinalizadas = new ArrayList<>();
+            this.listaDeNotasImportantes = new ArrayList<>();
         }
 
         public void adicionarNota(Notas nota) {
             listaDeNotas.add(nota);
-        
+        }
+        public void adicionarNotaFinalizada(NotaFinalizada notaFinalizada) {
+            listaDeNotasFinalizadas.add(notaFinalizada);
+        }
+        public void adicionarNotaImportante(NotaImportante notaImportante) {
+            listaDeNotasImportantes.add(notaImportante);
+        }
 
         public void exibirNotas() {
             System.out.println("Categoria: " + nomeCategoria);
             for (Notas nota : listaDeNotas) {
                 System.out.println(nota);
             }
+            for (NotaFinalizada notaFinalizada : listaDeNotasFinalizadas){
+                System.out.println(notaFinalizada);
+            }
+            for (NotaImportante notaImportante: listaDeNotasImportantes){
+                System.out.println(listaDeNotasImportantes);
+            }
         }
     }
 
-    abstract class Arquivo{
+    abstract class Arquivo {
         abstract void exportarArquivo();
         abstract void importarArquivo();
     }
-}
-
